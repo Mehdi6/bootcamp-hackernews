fc1c2d8e9965c3fda1d3e6c2a41fc8a3eec098ab
Mehdi ESSEBBAR <mehdiessebbar@gmail.com>
2018-03-28 17:40:47 +0100

    fixed untracked files

--- home/admin.py
+++
@@ -1,5 +0,0 @@
-#from django.contrib import admin
-#from .models import Topic, Comment
-# Register your models here.
-# from django.contrib.auth.admin import UserAdmin
-#
--- services/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Topic, Comment
-# Register your models here.
-# from django.contrib.auth.admin import UserAdmin
-#
-admin.site.register(Topic)
-admin.site.register(Comment)
